653ced11da0eebf574c8c716945e9e271a570493
Nicolás Boattini <38139882+nicolasboattini@users.noreply.github.com>
2024-04-05 12:38:34 -0300

    problemitas
    como existen 2 instancias de los dados, el metodo MoverFicha se llama dos veces, y si bien se ejecuta con normalidad, a la hora de hacer calculos los hace duplicados

--- Assets/Scripts/ControlTablero.cs
+++ Assets/Scripts/ControlTablero.cs
@@ -23,17 +23,21 @@ public class ControlTablero : MonoBehaviour
         for (int i = 0; i <= count; i++)
         {
             yield return new WaitForSeconds(0.5f);//delay
-            arrayFichas[Ficha].transform.position = arrayTablero[i].transform.position;
+            arrayFichas[Ficha].transform.position = arrayTablero[arrayFichas[Ficha].GetComponent<Ficha>().posActual+i].transform.position;
+            
             if (arrayTablero[i].name == "Laguna")
             {
                 Debug.Log("Laguna rotando");
                 arrayFichas[Ficha].transform.Rotate(0, 45, 0);
             }
         }
+        Debug.Log("Count: " + count);
+        arrayFichas[Ficha].GetComponent<Ficha>().posActual += count;        
         Debug.Log("Ya movi ficha, ahora voy a acomodar");
         Debug.Log("Accediendo al casillero: " + arrayTablero[count].name);
-
+        yield return new WaitForSeconds(0.5f);
         arrayTablero[count].GetComponent<Casillero>().AcomodarFicha(Ficha);
+        arrayFichas[Ficha].GetComponent<Ficha>().posActual = arrayFichas[Ficha].GetComponent<Ficha>().posActual/2;
     }
 }
 
